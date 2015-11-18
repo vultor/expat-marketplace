ac30e85ee8d0c952b3c27975264c613e5d56c54b
Vultor <shawn@shawnroe.com>
2015-11-18 12:21:54 +0900

    Includes Contants
    config, constants, and functions.php files.

---
+++ includes/config.php
@@ -0,0 +1,25 @@
+<?php
+
+    /**
+     * config.php
+     *
+     * Expat to Expat Marketplace
+     * by Shawn Roe
+     *
+     * Based off Computer Science 50
+     * Problem Set 7: CS50 Finance
+     *
+     * Configures pages.
+     */
+
+    // display errors, warnings, and notices
+    ini_set("display_errors", true);
+    error_reporting(E_ALL);
+
+    // requirements
+    require("constants.php");
+    require("functions.php");
+
+    // enable sessions
+    session_start();
+?>
---
+++ includes/constants.php
@@ -0,0 +1,24 @@
+<?php
+
+    /**
+     * constants.php
+     *
+     * Expat to Expat Marketplace
+     * by Shawn Roe
+     *
+     * Global constants.
+     */
+
+    // your database's name
+    define("DATABASE", "sroeco_expatmarketplace");
+
+    // your database's password
+    define("PASSWORD", "root");
+
+    // your database's server
+    define("SERVER", "localhost");
+
+    // your database's username
+    define("USERNAME", "root");
+
+?>
